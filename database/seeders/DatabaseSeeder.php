fe94cdc5094b5ee449095930d7d565a4bfa0fb0b
LinnPyaePyae <linnpyae175@gmail.com>
2023-08-04 14:32:58 +0630

    sales_seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\VoucherRecord;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             UserSeeder::class,
+            VoucherSeeder::class,
+            VoucherRecordSeeder::class
         ]);
     }
 }
