fb33e5b9217114cdd821fc885942bde53734dde6
justachillguy <naingyanzwe3002@gmail.com>
2023-08-24 15:05:49 +0630

    idkwthf

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,14 +12,14 @@ class ProfileController extends Controller
     public function chgPassword(Request $request)
     {
         $request->validate([
-            "current_password"=>"required|min:8",
-            "new_password"=>"required|confirmed",
+            "current_password" => "required|min:8",
+            "new_password" => "required|confirmed",
         ]);
 
         $userPassword = Auth::user()->password;
         //custom current_password with auth user password
-        if(!Hash::check($request->current_password,$userPassword)){
-            return redirect()->back()->withErrors(['current_password'=>'Password does not match']);
+        if (!Hash::check($request->current_password, $userPassword)) {
+            return redirect()->back()->withErrors(['current_password' => 'Password does not match']);
         }
 
         //update new password
@@ -37,47 +37,47 @@ class ProfileController extends Controller
     }
 
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
 
         $request->validate([
-            "name" => ["nullable","string", "min:3"],
-            "phone_number" => ["nullable","string"],
+            "name" => ["nullable", "string", "min:3"],
+            "phone_number" => ["nullable", "string"],
             "date_of_birth" => ["nullable", "date"],
             "gender" => ["nullable", "in:male,female"],
-            "address" => ["nullable","string", "max:300"],
-            "photo" => ['nullable','string',]
+            "address" => ["nullable", "string", "max:300"],
+            "photo" => ['nullable', 'string',]
 
         ]);
 
         $user = User::findOrFail($id);
 
-        if($request->has('name')){
+        if ($request->has('name')) {
             $user->name = $request->name;
         }
 
-        if($request->has('phone_number')){
+        if ($request->has('phone_number')) {
             $user->phone_number = $request->phone_number;
         }
 
-        if($request->has('date_of_birth')){
+        if ($request->has('date_of_birth')) {
             $user->date_of_birth = $request->date_of_birth;
         }
 
-        if($request->has('gender')){
+        if ($request->has('gender')) {
             $user->gender = $request->gender;
         }
 
-        if($request->has('address')){
+        if ($request->has('address')) {
             $user->address = $request->address;
         }
 
-        if($request->has('photo')){
+        if ($request->has('photo')) {
             $user->photo = $request->photo;
         }
 
         $user->update();
 
-        return response()->json(['message'=>"user's info is updated"]);
+        return response()->json(['message' => "user's info is updated"]);
     }
 }
