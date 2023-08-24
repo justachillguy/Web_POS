288ad3a1eb1f494f04555465013f28d1b152fbbd
justachillguy <naingyanzwe3002@gmail.com>
2023-08-25 00:01:13 +0630

    sales added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            PhotoSeeder::class,
+            // PhotoSeeder::class,
             UserSeeder::class,
             BrandSeeder::class,
             ProductSeeder::class,
