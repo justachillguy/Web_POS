78b7e9f52ff5c1614b36f79418168922a988977e
justachillguy <naingyanzwe3002@gmail.com>
2023-10-13 02:44:15 +0630

    update user seeder 4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            // UserSeeder::class,
+            UserSeeder::class,
             BrandSeeder::class,
             ProductSeeder::class,
             StockSeeder::class,
