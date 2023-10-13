e8f3565f0a98821320a12fdbbbd7b14481102af5
justachillguy <naingyanzwe3002@gmail.com>
2023-10-14 00:40:36 +0630

    fix stock index

--- app/Http/Resources/StockResource.php
+++ app/Http/Resources/StockResource.php
@@ -14,12 +14,10 @@ class StockResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $prodName = $this->product->name;
-        $brandName = $this->product->brand->name;
         return [
             "id" => $this->id,
-            "product_name" => $prodName,
-            "brand_name" => $brandName,
+            "product_name" => $this->product->name,
+            "brand_name" => $this->product->brand->name,
             "quantity" => $this->quantity,
         ];
     }
