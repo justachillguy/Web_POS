9056f5e267f39521792e98ebdc68a581f9f1de92
justachillguy <naingyanzwe3002@gmail.com>
2023-10-14 15:59:54 +0630

    update stock migrate add cascade on delete

--- database/migrations/2023_08_04_064837_create_stocks_table.php
+++ database/migrations/2023_08_04_064837_create_stocks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->foreignId('product_id');
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
             $table->integer('quantity');
             // $table->text('more')->nullable();
             $table->timestamps();
